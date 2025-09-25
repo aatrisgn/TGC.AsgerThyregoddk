edef10124ff6bd873050a98729574b9cfa71754b
Asger Thyregod <asger.thyregod@gmail.com>
2025-09-25 20:32:16 +0200

    Only deploying DNS now

--- terraform/envs/vars.dev.tfvars
+++
@@ -1,2 +0,0 @@
-resource_group_name        = "rg-asgerthyregoddk-dev-westeurope"
-environment_type_name      = "dev"
--- terraform/local.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  resource_location_name = replace(lower(var.environment_type_name), " ", "")
-  dns_zone_resource_name = lower(var.environment_type_name) == "prd" ? "asgerthyregod.dk" : "${var.environment_type_name}.asgerthyregod.dk"
-}
