70ed38382714cc264df2c4a6e17476ae898c3dd0
Asger Thyregod <asger.thyregod@gmail.com>
2025-09-25 20:44:26 +0200

    Added A record

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,11 @@ resource "azurerm_dns_zone" "dns_zone" {
     prevent_destroy = true
   }
 }
+
+resource "azurerm_dns_a_record" "all" {
+  ttl                 = 3600
+  name                = "*"
+  records             = ["20.16.81.112"]
+  zone_name           = azurerm_dns_zone.dns_zone.name
+  resource_group_name = data.azurerm_resource_group.predefined_resource_group.name
+}
