59d92d4a2002886fe172b836dafce814a0acee2b
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-04-12 20:03:50 +0200

    Added some personal stuff

---
+++ terraform/deployment/main.tf
@@ -0,0 +1,32 @@
+resource "azurerm_dns_zone" "dns_zone" {
+  name                = "asgerthyregod.dk"
+  resource_group_name = data.azurerm_resource_group.predefined_resource_group.name
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_dns_a_record" "apex" {
+  ttl                 = 3600
+  name                = "@"
+  records             = ["137.117.132.68"]
+  zone_name           = azurerm_dns_zone.dns_zone.name
+  resource_group_name = data.azurerm_resource_group.predefined_resource_group.name
+}
+
+resource "azurerm_dns_a_record" "dev" {
+  ttl                 = 3600
+  name                = "dev"
+  records             = ["137.117.132.68"]
+  zone_name           = azurerm_dns_zone.dns_zone.name
+  resource_group_name = data.azurerm_resource_group.predefined_resource_group.name
+}
+
+resource "azurerm_dns_a_record" "tst" {
+  ttl                 = 3600
+  name                = "tst"
+  records             = ["137.117.132.68"]
+  zone_name           = azurerm_dns_zone.dns_zone.name
+  resource_group_name = data.azurerm_resource_group.predefined_resource_group.name
+}
