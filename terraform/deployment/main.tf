2c18787780306296dd22e6975bf0b0aadc5ad35e
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-04-12 20:47:03 +0200

    Trying to apply

--- terraform/deployment/main.tf
+++ terraform/deployment/main.tf
@@ -2,3 +2,18 @@ resource "local_file" "deployment" {
   content  = data.local_file.deployment.content
   filename = "${var.destination_path}/deployment.yaml"
 }
+
+resource "local_file" "httproute" {
+  content  = data.local_file.deployment.content
+  filename = "${var.destination_path}/httproute.yaml"
+}
+
+resource "local_file" "service" {
+  content  = data.local_file.deployment.content
+  filename = "${var.destination_path}/service.yaml"
+}
+
+resource "local_file" "serviceaccount" {
+  content  = data.local_file.deployment.content
+  filename = "${var.destination_path}/serviceaccount.yaml"
+}
