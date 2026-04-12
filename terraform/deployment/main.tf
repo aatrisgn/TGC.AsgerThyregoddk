8df44bd825ffb704f9a98b0a4f2d5b98ea975772
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-04-12 20:53:35 +0200

    Fixed bad error in tf

--- terraform/deployment/main.tf
+++ terraform/deployment/main.tf
@@ -4,16 +4,16 @@ resource "local_file" "deployment" {
 }
 
 resource "local_file" "httproute" {
-  content  = data.local_file.deployment.content
+  content  = data.local_file.httproute.content
   filename = "${var.destination_path}/httproute.yaml"
 }
 
 resource "local_file" "service" {
-  content  = data.local_file.deployment.content
+  content  = data.local_file.service.content
   filename = "${var.destination_path}/service.yaml"
 }
 
 resource "local_file" "serviceaccount" {
-  content  = data.local_file.deployment.content
+  content  = data.local_file.serviceaccount.content
   filename = "${var.destination_path}/serviceaccount.yaml"
 }
