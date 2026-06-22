82a95c76fafed3e09d4f9b82e1b2cb31267b50c4
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-06-22 22:16:47 +0200

    Trying to do stuff

--- terraform/infrastructure/data.tf
+++ terraform/infrastructure/data.tf
@@ -1,7 +1,3 @@
 data "scaleway_account_project" "default_project" {
   name = var.project_name
 }
-
-data "scaleway_domain_registration" "root_domain_registration" {
-  domain_name = "asgerthyregod.dk"
-}
--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -1,18 +1,26 @@
-resource "scaleway_domain_registration" "domain_registration" {
-  project_id   = data.scaleway_account_project.default_project.id
-  domain_names = ["asgerthyregod.dk"]
+# resource "scaleway_domain_registration" "domain_registration" {
+#   project_id   = data.scaleway_account_project.default_project.id
+#   domain_names = ["asgerthyregod.dk"]
 
-  owner_contact {
-    legal_form                  = "individual"
-    firstname                   = "John"
-    lastname                    = "DOE"
-    email                       = "john.doe@example.com"
-    phone_number                = "+1.23456789"
-    address_line_1              = "123 Main Street"
-    city                        = "Paris"
-    zip                         = "75001"
-    country                     = "FR"
-    vat_identification_code     = "FR12345678901"
-    company_identification_code = "123456789"
-  }
+#   owner_contact {
+#     legal_form                  = "individual"
+#     firstname                   = "John"
+#     lastname                    = "DOE"
+#     email                       = "john.doe@example.com"
+#     phone_number                = "+1.23456789"
+#     address_line_1              = "123 Main Street"
+#     city                        = "Paris"
+#     zip                         = "75001"
+#     country                     = "FR"
+#     vat_identification_code     = "FR12345678901"
+#     company_identification_code = "123456789"
+#   }
+# }
+
+resource "scaleway_domain_record" "www2" {
+  dns_zone = "asgerthyregod.dk"
+  name     = "test"
+  type     = "A"
+  data     = "1.2.3.5"
+  ttl      = 3600
 }
