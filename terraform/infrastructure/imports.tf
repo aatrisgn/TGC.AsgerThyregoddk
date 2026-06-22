1d17fce2957fb2bbb64a4481e9171c06d4858dec
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-06-22 22:38:32 +0200

    Clean-up and added domains

--- terraform/infrastructure/imports.tf
+++
@@ -1,4 +0,0 @@
-# import {
-#   to = scaleway_domain_registration.domain_registration
-#   id = data.scaleway_domain_registration.root_domain_registration.task_id
-# }
--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -1,26 +1,9 @@
-# resource "scaleway_domain_registration" "domain_registration" {
-#   project_id   = data.scaleway_account_project.default_project.id
-#   domain_names = ["asgerthyregod.dk"]
-
-#   owner_contact {
-#     legal_form                  = "individual"
-#     firstname                   = "John"
-#     lastname                    = "DOE"
-#     email                       = "john.doe@example.com"
-#     phone_number                = "+1.23456789"
-#     address_line_1              = "123 Main Street"
-#     city                        = "Paris"
-#     zip                         = "75001"
-#     country                     = "FR"
-#     vat_identification_code     = "FR12345678901"
-#     company_identification_code = "123456789"
-#   }
-# }
-
 resource "scaleway_domain_record" "www2" {
+  for_each = toset(local.cluster_domains)
+
   dns_zone = "asgerthyregod.dk"
-  name     = "test"
+  name     = each.value
   type     = "A"
-  data     = "1.2.3.5"
+  data     = "137.117.132.68"
   ttl      = 3600
 }
