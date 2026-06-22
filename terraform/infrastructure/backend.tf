35b3fc51e7c9631757f34b0387ae866e7f96fd6b
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-06-22 21:23:26 +0200

    Re-adding backend

--- terraform/infrastructure/backend.tf
+++ terraform/infrastructure/backend.tf
@@ -1,3 +1,12 @@
-data "scaleway_account_project" "default_project" {
-  name = "landing-zone-${var.environment}"
+terraform {
+  backend "s3" {
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+
+    region = "fr-par"
+    endpoints = {
+      s3 = "https://s3.fr-par.scw.cloud"
+    }
+  }
 }
